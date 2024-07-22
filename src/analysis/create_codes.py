c17aa2f22b40096cd43c4f38ba7a7f16dc31e5d8
Vansh <vanshjjw@gmail.com>
2024-07-22 19:55:29 +0100

    known codes in file format for ease of use

--- src/analysis/create_codes.py
+++ src/analysis/create_codes.py
@@ -1,83 +1,57 @@
+import json
 import numpy as np
-import src.helpers as helper
-import src.core as code
+import src.core as core
 import os
 
 
-def interesting_cases(data: dict):
-    data["0"] = {
-        "l": 6,
-        "m": 9,
-        "a": ["x3", "y1", "y2"],
-        "b": ["y3", "x2", "x4"],
-        "answer": [[108, 16, 6]]
-    }
-    data["1"] = {
+def raw_data():
+    # add data to this dictionary in the format of the examples below
+    # run add_data_to_json() to save data to the known codes file
+    new_codes = {}
+    new_codes["0"] = {
         "l": 6,
         "m": 9,
         "a": ["x0", "y1", "y2"],
         "b": ["y3", "x2", "x4"],
-        "answer": [[108, 16, 6]]
+        "answer": [10844, 16, 6]
     }
-
-
-def raw_data(data: dict):
-    data["0"] = {
-        "l": 6,
-        "m": 9,
-        "a": ["x0", "y1", "y2"],
-        "b": ["y3", "x2", "x4"],
-        "answer": [[108, 16, 6]]
-    }
-    data["1"] = {
+    new_codes["1"] = {
         "l": 9,
         "m": 9,
         "a": ["x0", "x1", "y1"],
         "b": ["x3", "y1", "y2"],
-        "answer": [[162, 4, 16]]
+        "answer": [162444, 4, 16]
     }
-    data["2"] = {
-        "l": 9,
-        "m": 9,
-        "a": ["x0", "x1", "y6"],
-        "b": ["y3", "x2", "x3"],
-        "answer": [[162, 12, 8]]
-    }
-    data["3"] = {
-        "l": 9,
-        "m": 9,
-        "a": ["x0", "y1", "y2"],
-        "b": ["y3", "x3", "x6"],
-        "answer": [[162, 24, 6]]
-    }
-    data["4"] = {
-        "l": 9,
-        "m": 15,
-        "a": ["x3", "y1", "y2"],
-        "b": ["y3", "x1", "x2"],
-        "answer": [[270, 8, 18]]
-    }
-    data["5"] = {
-        "l": 7,
-        "m": 7,
-        "a": ["x1", "y3", "y4"],
-        "b": ["y1", "x3", "x4"],
-        "answer": [[98, 6, 12]]
-    }
-    data["6"] = {
-        "l": 9,
-        "m": 9,
-        "a": ["x3", "y1", "y2"],
-        "b": ["y3", "x1", "x2"],
-        "answer": [[162, 8, 12]]
-    }
-    data["7"] = {
-        "l": 8,
-        "m": 8,
-        "a": ["x2", "y1", "y3", "y4"],
-        "b": ["y2", "x1", "x2", "x4"],
-        "answer": [[128, 14, 12]]
-    }
+    return new_codes
+
+
+def save_raw_data_file(data):
+    with open("known_codes", "w") as file:
+        file.write("{\n")
+        for i, key in enumerate(data):
+            file.write(f"\"{key}\": {str(data[key])}")
+            if i < len(data) - 1:
+                file.write(",")
+            file.write("\n")
+        file.write("}")
+        file.flush()
+        file.close()
+
+
+def add_raw_data_to_json():
+    with open("known_codes", "r") as file:
+        old_codes = json.loads(file.read().replace("\'", "\""))
+        new_codes = raw_data()
+
+        num_codes = len(old_codes)
+        num_codes_to_add = len(new_codes)
+
+        for i in range(num_codes, num_codes + num_codes_to_add):
+            if str(i) in old_codes:
+                raise ValueError(f"code numbered {i} already exists")
+            old_codes[str(i)] = new_codes[str(i - num_codes)]
+
+    save_raw_data_file(old_codes)
 
 
 def write_matrix(matrix, file):
@@ -89,51 +63,51 @@ def write_matrix(matrix, file):
 
 
 # TODO - wsl does not naturally have the path to Results directory saved. Need to give it the relative path from root
-def save_results():
-    Main = {}
-    raw_data(Main)
+def run_and_save_results():
+    with open("known_codes", "r") as file:
+        data = json.loads(file.read().replace("\'", "\""))
 
-    for key in Main:
-        example = Main[key]
+    save_as_numpy = True
+
+    for key in data:
+        example = data[key]
         l = example["l"]
         m = example["m"]
         a = example["a"]
         b = example["b"]
-        c=example["answer"]
+        c = example["answer"]
 
-        obj = code.BBCode(l, m, a, b, debug=False)
+        obj = core.BBCode(l, m, a, b, debug=False)
         n, k, d = obj.generate_bb_code(distance_method=3)
         H_x, H_z = obj.create_parity_check_matrices()
 
-        folder = 'Results_npz'
-        file_name = f"{c}.npz"
-        os.makedirs(folder, exist_ok=True)
-        file_path = os.path.join(folder, file_name)
 
-        with open(file_path, 'wb') as file:
-            np.savez(file, Hx=H_x, Hz=H_z)
+        if save_as_numpy:
+            file_path = os.path.join('Results_npz', f"[[{n}.{k},{d}]].npz")
+            with open(file_path, 'wb') as file:
+                np.savez(file, Hx = H_x, Hz = H_z)
+            return
 
-        # with open(f"Results/[[{n}.{k},{d}]].txt", 'wb') as file:
-        #     file.write(f"l: {l}\n")
-        #     file.write(f"m: {m}\n")
-        #     file.write(f"A: {a}\n")
-        #     file.write(f"B: {b}\n")
-        #
-        #     file.write(f"\nBB code: [{n}, {k}, {d}]\n\n")
-        #
-        #     if "answer" in example:
-        #         file.write(f"\nKnown BB code: {example['answer']}\n\n")
-        #
-        #     file.write("H_x:\n")
-        #     write_matrix(H_x, file)
-        #     file.write("H_z:\n")
-        #     write_matrix(H_z, file)
-        #     file.flush()
-        #     file.close()
+        with open(f"Results/[[{n}.{k},{d}]].txt", 'wb') as file:
+            file.write(f"l: {l}\n")
+            file.write(f"m: {m}\n")
+            file.write(f"A: {a}\n")
+            file.write(f"B: {b}\n")
+
+            file.write(f"\nBB code: [{n}, {k}, {d}]\n\n")
+
+            if "answer" in example:
+                file.write(f"\nKnown BB code: {example['answer']}\n\n")
+
+            file.write("H_x:\n")
+            write_matrix(H_x, file)
+            file.write("H_z:\n")
+            write_matrix(H_z, file)
+            file.flush()
+            file.close()
 
 
 
 
 if __name__ == "__main__":
-    save_results()
-    print("Results saved.")
+    pass
