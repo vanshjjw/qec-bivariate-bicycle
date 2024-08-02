970cf0de40f4430002ff6665a5682d8d1a01b4af
Vansh <vanshjjw@gmail.com>
2024-08-02 20:17:31 +0100

    helpers edited

--- src/helpers.py
+++ src/helpers.py
@@ -2,6 +2,10 @@ import numpy as np
 from ldpc.mod2 import reduced_row_echelon, rank
 from copy import deepcopy
 
+
+## ----------------- QEC Specific Functions ----------------- ##
+
+
 def generators(G_standard):
     G1=G_standard[:][:G_standard.shape[1]//2]
     G2=G_standard[:][G_standard.shape[1]//2:]
@@ -13,7 +17,6 @@ def generators(G_standard):
 def standard_form(H_x, H_z):
     G = make_block_diagonal(H_x, H_z)
     n, m = G.shape[1] // 2, G.shape[0]
-    k = n - m
 
     G1 = G[:, :n]
     G2 = G[:, n:]
@@ -32,47 +35,9 @@ def standard_form(H_x, H_z):
     G2[r:,:r] = D
     G2[:r,r:] = C
 
-    G_new = np.hstack((G1_rref, G2))
-    G_standard = G_new[ ~ np.all(G_new == 0, axis=1)]
-
+    G_complete = np.hstack((G1_rref, G2))
+    G_standard = G_complete[ ~ np.all(G_complete == 0, axis=1)]
     return G_standard
-
-
-def display(M, middle_line = False):
-    size = len(M[0])
-
-    h_line = "--" * size
-    print(h_line)
-
-    for row in M:
-        print("[ ", end = "")
-        for i in range(size):
-            print(row[i], end = " ")
-            if middle_line and i == size / 2:
-                print("|", end = " ")
-        print("]")
-
-    print(h_line)
-    pass
-
-
-def binary_rank(A):
-    return rank(A)
-
-
-def hamming_weight(vector: list[int]):
-    # hamming weight for a CSS src logical operator (length = 2n)
-    n = len(vector) // 2
-    weight = sum([1 if vector[i] == 1 or vector[i + n] == 1 else 0 for i in range(n)])
-    return weight
-
-
-def make_block_diagonal(H_x, H_z):
-    G = np.block([
-        [H_x, np.zeros((H_x.shape[0], H_z.shape[1]), dtype=int)],
-        [np.zeros((H_z.shape[0], H_x.shape[1]), dtype=int), H_z]
-    ])
-    return G
 
 
 def find_logical_generators(G_standard, rank_x: int) -> list[np.ndarray]:
@@ -93,6 +58,9 @@ def find_logical_generators(G_standard, rank_x: int) -> list[np.ndarray]:
     Lz = np.hstack((zero_l, zero_m, zero_r, A2.T, zero_m, identity))
 
     return Lx, Lz
+
+
+## ----------------- Polynomial Helpers ----------------- ##
 
 
 def construct_answer(x_power: int, y_power: int):
@@ -121,3 +89,42 @@ def multiply_polynomials_mod_2(poly1: list[str], poly2: list[str], l: int, m: in
                 result.append(answer)
 
     return result
+
+
+## ----------------- Matrix Helpers ----------------- ##
+
+def display(M, middle_line = False):
+    size = len(M[0])
+
+    h_line = "--" * size
+    print(h_line)
+
+    for row in M:
+        print("[ ", end = "")
+        for i in range(size):
+            print(row[i], end = " ")
+            if middle_line and i == size / 2:
+                print("|", end = " ")
+        print("]")
+
+    print(h_line)
+    pass
+
+
+def hamming_weight(vector: list[int]):
+    # hamming weight for a CSS src logical operator (length = 2n)
+    n = len(vector) // 2
+    weight = sum([1 if vector[i] == 1 or vector[i + n] == 1 else 0 for i in range(n)])
+    return weight
+
+
+def make_block_diagonal(H_x, H_z):
+    G = np.block([
+        [H_x, np.zeros((H_x.shape[0], H_z.shape[1]), dtype=int)],
+        [np.zeros((H_z.shape[0], H_x.shape[1]), dtype=int), H_z]
+    ])
+    return G
+
+
+def binary_rank(A):
+    return rank(A)
