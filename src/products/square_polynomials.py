8f3096f9d825315a748df8a92b0e39afcc2ff011
Vansh <vanshjjw@gmail.com>
2024-08-13 14:01:56 +0100

    cheeky nonsense

--- src/products/square_polynomials.py
+++ src/products/square_polynomials.py
@@ -1,5 +1,5 @@
 from src.propose_parameters import ProposeParameters
-from src.polynomials import PolynomialToGraphs
+from src.polynomials import PolynomialToGraphs, PolynomialHelper
 from src.core import BBCode
 from src.core_cached import BBCodeCached
 import numpy as np
@@ -10,7 +10,7 @@ def change_parameters():
     l = np.random.randint(8, 14)
     m = np.random.randint(8, 14)
     parameters = ProposeParameters(l, m)
-    poly_graph = PolynomialToGraphs(l, m)
+    poly_graph = PolynomialHelper(l, m)
     code_cached = BBCodeCached(l, m)
     print(f"New parameters: l = {l}, m = {m}")
     print("\n")
@@ -24,7 +24,7 @@ def square_connected_polynomials():
     num_x = 3
     num_y = 3
     parameters = ProposeParameters(l, m)
-    poly_graph = PolynomialToGraphs(l, m)
+    poly_help = PolynomialHelper(l, m)
     code_cached = BBCodeCached(l, m)
 
     zero_k = 0
@@ -40,11 +40,11 @@ def square_connected_polynomials():
 
         # change parameters every 200 shots
         if i % 200 == 0 and i != 0:
-            parameters, poly_graph, code_cached = change_parameters()
+            parameters, poly_help, code_cached = change_parameters()
 
         # create base polynomials
         A, B = parameters.distribute_monomials(parameters.draw_random_monomials(num_x, num_y))
-        n1, k1, d1 = code_cached.set_expressions(A, B).generate_bb_code(distance_method=4)
+        n1, k1, d1 = code_cached.set_expressions(A, B).generate_bb_code(distance_method=3)
 
         if k1 == 0:
             zero_k += 1
@@ -53,46 +53,103 @@ def square_connected_polynomials():
         graph_1 = code_cached.graph()
         num_components_1 = helper.num_connected_components(graph_1)
 
-        # base polynomials gives non-zero k
-        print(f"A: {A}, B: {B}")
-        print(f"code: [{n1}, {k1}, {d1}]")
-        print(f"Number of components in original polynomials: {num_components_1}")
-        print("\n")
-
         ### square polynomials
-        A2 = poly_graph.poly_help.multiply_polynomials(A, A)
-        B2 = poly_graph.poly_help.multiply_polynomials(B, B)
+        A2 = poly_help.multiply_polynomials(A, A)
+        B2 = poly_help.multiply_polynomials(B, B)
 
-        n2, k2, d2 = code_cached.set_expressions(A2, B2).generate_bb_code(distance_method=4)
+        n2, k2, d2 = code_cached.set_expressions(A2, B2).generate_bb_code(distance_method=3)
         graph_2 = code_cached.graph()
         num_components_2 = helper.num_connected_components(graph_2)
 
-        # print squared polynomials results
-        print(f"new code: [{n2}, {k2}, {d2}]")
-        print(f"Number of components in squared polynomials: {num_components_2}")
-
-        print("\n")
-        print(f"k increased by : {(k2 / k1)}", end=": ")
-        print(f"components increased by: {num_components_2 / num_components_1}")
-
         increase_equally = (k2 / k1) == (num_components_2 / num_components_1)
 
-        if num_components_2 == 1:
-            print("Squared polynomials are connected", end=": ")
-            if increase_equally:
-                print("conjecture passes")
-            else:
-                print("conjecture fails")
-        else:
-            print("Squared polynomials are disconnected", end=": ")
-            if increase_equally:
-                print("conjecture passes")
-            else:
-                print("conjecture fails")
-        print("\n")
+        if not increase_equally:
+            # base polynomials gives non-zero k
+            print("\n")
+            print(f"A: {A}, B: {B}")
+            print(f"code: [{n1}, {k1}, {d1}]")
+            print(f"Number of components in original polynomials: {num_components_1}")
+            print("\n")
 
-        print("------------\n\n")
+            # print squared polynomials results
+            print(f"new code: [{n2}, {k2}, {d2}]")
+            print(f"Number of components in squared polynomials: {num_components_2}")
+            print("\n")
+            print(f"k increased by : {(k2 / k1)}", end=": ")
+            print(f"components increased by: {num_components_2 / num_components_1}")
+            print("------------\n\n")
+        else:
+            which_one = "connected" if num_components_2 == 1 else "disconnected"
+            print(f"conjecture holds for {which_one} polynomials")
+
+
+
+def cheeky_examples():
+    l = 8
+    m = 8
+    A = ['x', 'y2', 'i']
+    B = ['y', 'x2', 'i']
+
+    poly_help = PolynomialHelper(l, m)
+    code = BBCode(l, m, A, B)
+    n, k, d = code.generate_bb_code(distance_method=3)
+    print(f"A: {A}, B: {B}")
+    print(f"code: [{n}, {k}, {d}]")
+    print("\n")
+
+    l = 10
+    m = 10
+    A = ['x', 'y2', 'i']
+    B = ['y', 'x2', 'i']
+
+    poly_help = PolynomialHelper(l, m)
+    code = BBCode(l, m, A, B)
+    n, k, d = code.generate_bb_code(distance_method=3)
+    print(f"A: {A}, B: {B}")
+    print(f"code: [{n}, {k}, {d}]")
+    print("\n")
+
+    l = 12
+    m = 12
+    A = ['x', 'y2', 'i']
+    B = ['y', 'x2', 'i']
+
+    poly_help = PolynomialHelper(l, m)
+    code = BBCode(l, m, A, B)
+    n, k, d = code.generate_bb_code(distance_method=3)
+    print(f"A: {A}, B: {B}")
+    print(f"code: [{n}, {k}, {d}]")
+    print("\n")
+
+    l = 14
+    m = 14
+    A = ['x', 'y2', 'i']
+    B = ['y', 'x2', 'i']
+
+    poly_help = PolynomialHelper(l, m)
+    code = BBCode(l, m, A, B)
+    n, k, d = code.generate_bb_code(distance_method=3)
+    print(f"A: {A}, B: {B}")
+    print(f"code: [{n}, {k}, {d}]")
+    print("\n")
 
 
 if __name__ == "__main__":
-    square_connected_polynomials()
+    cheeky_examples()
+    pass
+
+
+
+
+
+# A: ['x2', 'y11', 'x9'], B: ['y8', 'y9', 'x5']
+# code: [288, 12, 20]
+# Number of components in original polynomials: 1
+#
+#
+# new code: [288, 32, 6]
+# Number of components in squared polynomials: 4
+#
+#
+# k increased by : 2.6666666666666665: components increased by: 4.0
+# Squared polynomials are disconnected: conjecture fails
