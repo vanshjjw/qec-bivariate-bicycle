ed2dd322efb2134d7a6c7f41a7c0d1af2f375697
Vansh <vanshjjw@gmail.com>
2024-08-16 19:15:27 +0100

    explain please

--- src/symmetric.py
+++ src/symmetric.py
@@ -1,4 +1,4 @@
-from sage.all import *
+from sage.all import FreeGroup, Integer
 import numpy as np
 import re
 
@@ -30,41 +30,18 @@ def convert_string_to_expression(exp: list[str], gens):
         expression.append(eval(string, {}, variables))
     return expression
 
-# gen = ['r']
-# rel = ['r^10']
-#
-# F = FreeGroup(gen)
-# fgens = F._first_ngens(len(gen))
-# rel = convert_string_to_expression(rel,fgens)
-# G = F/rel
-# gens = G.gens()
-# print(G.list()[1]**10 == G.list()[0])
 
-# def convert_string_to_expression(exp: list[str], x, y):
-#     """
-#     x, y: generators of the group
-#
-#     The strings in exp are converted to expressions in x and y.
-#     """
-#     expression = []
-#     for string in exp:
-#         # Replace '^' with '**Integer(' and close the parenthesis
-#         string = re.sub(r'\^', '**Integer(', string)
-#         # Add closing parenthesis for Integer exponents
-#         string = re.sub(r'(\*\*Integer\(\-?\d+)', r'\1)', string)
-#         # Add Integer for standalone exponents like `y^-1`
-#         string = re.sub(r'([xy])(\-?\d+)', r'\1**Integer(\2)', string)
-#         expression.append(eval(string))
-#     return expression
+def make_group(generators: list[str], rel: list[str], a: list[str], b: list[str]):
+    F = FreeGroup(generators)
+    F_generators = F._first_ngens(len(generators))
+    relators = convert_string_to_expression(rel, F_generators)
 
-def make_group(gen: list[str], rel: list[str], a: list[str], b: list[str]):
-    F = FreeGroup(gen)
-    fgens = F._first_ngens(len(gen))
-    rel = convert_string_to_expression(rel,fgens)
-    G = F / rel
-    gens=G.gens()
-    a = convert_string_to_expression(a, gens)
-    b = convert_string_to_expression(b, gens)
+    G = F / relators
+    G_generators = G.gens()
+
+    a = convert_string_to_expression(a, G_generators)
+    b = convert_string_to_expression(b, G_generators)
+
     for i in range(len(a)):
         if a[i] == 1:
             a[i] = G.list()[0]
@@ -97,23 +74,24 @@ def block_matrix(G, alg, action: str):
 
 
 ## ----------------- Inputs ----------------- ##
-gen = ['x', 'y'] #list of group generators (two generators for Symmetric groups).
-rel = ['x^3', 'y^2', 'x*y^-1*x*y'] #group relators, must use the same generator names defined in gen.
 
-#group algebra elements used to make A and B matrices.
+generators = ['x', 'y'] #list of group generators (two generators for Symmetric groups).
+relators = ['x^3', 'y^2', 'x*y^-1*x*y'] # group relators, must use the same generator names defined in gen.
+
+# Group algebra elements used to make A and B matrices.
 a = ['1','x','x*y']
 b = ['1','y','y*x^2']
 
-# gen = ['x','y'] #list of group generators (two generators for Symmetric groups).
-# rel = ['x^9', 'y^4', 'y^-1*x*y*x']
-# a = ['1','x','y','y*x^6']
-# b = ['1','y^2*x','y^2*x^6','x^2']
 ## ----------------- Parity Check Matrices ----------------- ##
-G, a, b = make_group(gen, rel, a, b)
 
-#A always has action == 'left' and B always has action == right
-A=block_matrix(G, a, 'left')
-B=block_matrix(G, b, 'right')
+
+G, a, b = make_group(generators, relators, a, b)
+
+print(G)
+
+# A always has action == 'left' and B always has action == right
+A = block_matrix(G, a, 'left')
+B = block_matrix(G, b, 'right')
 
 H_x = np.hstack((A, B))
 H_z = np.hstack((B.T, A.T))
