3a1c944db88ba42336681368d1280e234234254f
Vansh <vanshjjw@gmail.com>
2024-08-20 17:42:02 +0100

    sym groups updated

--- Testing/bbcode_general.py
+++ Testing/bbcode_general.py
@@ -3,40 +3,33 @@ from src.core_general import BBCodeGeneral
 def raw_codes(codes: dict):
     codes["0"] = {
         "gen": ['x', 'y'],
-        "rel": ['x3', 'y2', 'x.y-1.x.y'],
-        "a": ['i', 'x', 'y.x2'],
-        "b": ['i', 'y', 'x.y'],
-        "answer": [12, 2, 2]
-    }
-    codes["1"] = {
-        "gen": ['x', 'y'],
         "rel": ['y4', 'x6', 'y-1.x.y.x'],
         "a": ['i', 'x'],
         "b": ['i', 'y', 'x6', 'y3.x', 'y.x7', 'y3.x5'],
-        "answer": [48, 8, 4]
+        "answer": [72, 8, 9]
     }
-    codes["2"] = {
+    codes["1"] = {
         "gen": ['x', 'y'],
         "rel": ['y5', 'x8', 'x-1.y.x.y'],
         "a": ['i', 'y.x4'],
         "b": ['i', 'x', 'x2', 'y', 'y3.x', 'y2.x6'],
-        "answer": [80,8,10]
+        "answer": [80, 8, 10]
     }
-    codes["3"] = {
+    codes["2"] = {
         "gen": ['x', 'y'],
         "rel": ['y3', 'x16', 'x-1.y.x.y'],
         "a": ['i', 'x', 'y', 'x14'],
         "b": ['i', 'x2', 'y.x4', 'x11'],
-        "answer": [96,8,12]
+        "answer": [96, 8, 12]
     }
-    codes["4"] = {
+    codes["3"] = {
         "gen": ['x', 'y'],
         "rel": ['y3', 'x14', 'x-1.y.x.y'],
         "a": ['i', 'x7', 'y.x10', 'x8'],
         "b": ['i', 'y', 'y2.x13', 'x5'],
-        "answer": [84,10,8]
+        "answer": [82, 10, 9]
     }
-    codes["5"] = {
+    codes["4"] = {
         "gen": ['x', 'y'],
         "rel": ['y4', 'x12', 'y-1.x.y.x'],
         "a": ['i', 'y', 'x9', 'y.x'],
@@ -44,31 +37,22 @@ def raw_codes(codes: dict):
         "answer": [96,10,12]
 
     }
-    codes["6"] = {
+    codes["5"] = {
         "gen": ['x', 'y'],
         "rel": ['y6', 'x8', 'x-1.y.x.y'],
         "a": ['i', 'x', 'y3.x2', 'y2.x3'],
         "b": ['i', 'x', 'y4.x6', 'y5.x3'],
-        "answer": [96,12,10]
+        "answer": [96, 12, 10]
+    }
+    codes["6"] = {
+        'gen' : ['x', 'y'],
+        'rel' : ['y4', 'x9', 'y-1.x.y.x'],
+        'a' : ['i', 'y', 'x', 'y.x6'],
+        'b' : ['i', 'y2.x', 'y2.x6', 'x2'],
+        'answer' : [72, 8, 9]
     }
     return codes
 
-def compare(codes: dict):
-    codes["0"] = {
-        "gen": ['x','y'],
-        "rel": None,
-        "group": 5,
-        "a": ['x3', 'y2','y7'],
-        "b": ['y3','x', 'x2']
-    }
-    codes["1"] = {
-        "gen": ['x', 'y'],
-        "rel": ['x12', 'y10', 'y-1.x.y.x-1'],
-        "group": None,
-        "a": ['i', 'y', 'x.y7'],
-        "b": ['i', 'x', 'x10.y4']
-    }
-    return codes
 
 def display_code(code, n, k, d):
     print(f"Generators: {code['gen']}")
@@ -89,7 +73,7 @@ def run_bbcode_general_examples():
 
     for i in range(len(Main)):
         example = Main[str(i)]
-        code = BBCodeGeneral(example["gen"], example["rel"], example["group"], safe_mode=True).set_expression(example["a"], example["b"])
+        code = BBCodeGeneral(example["gen"], example["rel"], safe_mode=True).set_expression(example["a"], example["b"])
 
         n, k, d = code.generate_bb_code(distance_method=distance_method)
         n_known, k_known, d_known = example["answer"]
@@ -101,32 +85,9 @@ def run_bbcode_general_examples():
         else:
             print(f"\n\nCode {i} failed. Details:")
             display_code(example, n, k, d)
-            return
-
-def compare_groups():
-    Main = {}
-    distance_method = 3
-    distance_margin = 1.15 # 15% margin of error
-
-    Main = compare(Main)
-
-    for i in range(len(Main)):
-        example = Main[str(i)]
-        code = BBCodeGeneral(example["gen"], example["rel"], example["group"], safe_mode=False).set_expression(example["a"], example["b"])
-
-        n, k, d = code.generate_bb_code(distance_method=distance_method)
-        # n_known, k_known, d_known = example["answer"]
-        #
-        # passed = n == n_known and k == k_known and d_known <= d * distance_margin
-        print(f"[[{n},{k},{d}]]")
-        # if passed:
-        #     print(f"[[{n},{k},{d}]]")
-        # else:
-        #     print(f"\n\nCode {i} failed. Details:")
-        #     display_code(example, n, k, d)
-        #     return
+            print("\n\n")
 
 
 
 if __name__ == "__main__":
-    compare_groups()
+    run_bbcode_general_examples()
