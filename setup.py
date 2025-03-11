0fd000153b08e79368bfbb3efc9ee9516d401c47
Vansh <vanshjjw@gmail.com>
2025-03-11 20:30:14 +0000

    package name change

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='error_correction',
+    name='qec-bicycle-bivariate',
     version='0.1',
     packages=find_packages(),
     install_requires=[
